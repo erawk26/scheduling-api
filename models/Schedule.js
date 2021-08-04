43538b444eeeba8f32bd793f16f81e4bf18bfc22
Erik Olsen <erik.olsen@milespartnership.com>
2021-08-04 17:03:47 -0400

    Schema change

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 const ScheduleSchema = new mongoose.Schema({
-    title: String,
-    description: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  address1: String,
+  address2: String,
+  city: String,
+  state: String,
+  zip: String,
+  schedule: Array,
+  datesArr: Array,
+  comments: String,
 });
 module.exports = mongoose.model("Schedule", ScheduleSchema);
