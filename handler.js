824df689c15f4b0896fc1900a55d3c7d572a0ffd
Erik Olsen <erik.olsen@milespartnership.com>
2021-08-04 09:06:49 -0400

    initial commit

---
+++ handler.js
@@ -0,0 +1,117 @@
+const connectToDatabase = require("./db");
+const Schedule = require("./models/Schedule");
+require('dotenv').config({ path: './.env' });
+
+("use strict");
+
+module.exports.create = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    connectToDatabase().then(() => {
+        Schedule.create(JSON.parse(event.body))
+            .then((schedule) =>
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(schedule),
+                })
+            )
+            .catch((err) =>
+                callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: { "Content-Type": "text/plain" },
+                    body: "Could not create the schedule.",
+                })
+            );
+    });
+};
+
+module.exports.getOne = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    connectToDatabase().then(() => {
+        Schedule.findById(event.pathParameters.id)
+            .then((schedule) =>
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(schedule),
+                })
+            )
+            .catch((err) =>
+                callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: { "Content-Type": "text/plain" },
+                    body: "Could not fetch the schedule.",
+                })
+            );
+    });
+};
+
+module.exports.getAll = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    connectToDatabase().then(() => {
+        Schedule.find()
+            .then((schedules) =>
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(schedules),
+                })
+            )
+            .catch((err) =>
+                callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: { "Content-Type": "text/plain" },
+                    body: "Could not fetch the schedules.",
+                })
+            );
+    });
+};
+
+module.exports.update = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    connectToDatabase().then(() => {
+        Schedule.findByIdAndUpdate(
+            event.pathParameters.id,
+            JSON.parse(event.body),
+            { new: true }
+        )
+            .then((schedule) =>
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(schedule),
+                })
+            )
+            .catch((err) =>
+                callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: { "Content-Type": "text/plain" },
+                    body: "Could not fetch the schedules.",
+                })
+            );
+    });
+};
+
+module.exports.delete = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    connectToDatabase().then(() => {
+        Schedule.findByIdAndRemove(event.pathParameters.id)
+            .then((schedule) =>
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        message: "Removed schedule with id: " + schedule._id,
+                        schedule: schedule,
+                    }),
+                })
+            )
+            .catch((err) =>
+                callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: { "Content-Type": "text/plain" },
+                    body: "Could not fetch the schedules.",
+                })
+            );
+    });
+};
